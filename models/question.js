cec9feddc706ec3e3fead4d5cc8d61b9b20ddb4a
Rahul Jha <rahoul.jha@ejamerica.com>
2023-10-14 13:54:35 +0530

    project ready for deployment

---
+++ models/question.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose")
+
+const questionSchema = mongoose.Schema({
+	content: {
+		type: String,
+		required: true
+	},
+	imageName: {
+		type: String,
+		required: true
+	},
+	video: {
+		type: String,
+		required: true
+	},
+	correctanswer: {
+		type: String,
+		required: true
+	},
+	quetype: {
+		type: String,
+		required: true
+	},
+	examtype: {
+		type: String,
+		required: true
+	},
+	difficultylevel: {
+		type: String,
+		required: true
+	},
+
+	creationDate: {
+		type: Date,
+		required: true,
+		default: Date.now
+	},
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "User"
+	},
+	topic: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "Topic"
+	},
+	answers: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Answer"
+		}
+	]
+})
+
+module.exports = mongoose.model("Question", questionSchema)
